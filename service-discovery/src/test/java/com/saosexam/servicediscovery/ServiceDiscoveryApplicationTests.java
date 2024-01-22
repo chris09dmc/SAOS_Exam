78e60f2fe9fecfba07628997ea5e0083207ba54c
chris09dmc <98541925+chris09dmc@users.noreply.github.com>
2024-01-22 16:28:20 +0100

    Add files via upload

---
+++ service-discovery/src/test/java/com/saosexam/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saosexam.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
