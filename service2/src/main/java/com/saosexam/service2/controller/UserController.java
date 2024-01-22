68cfaadc1bb8d0cdf2c796f38d61f2665db0b3a4
chris09dmc <98541925+chris09dmc@users.noreply.github.com>
2024-01-22 16:27:32 +0100

    Add files via upload

---
+++ service2/src/main/java/com/saosexam/service2/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.saosexam.service2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/user-page")
+    public String userPage (Model model, Principal principal) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(principal.getName());
+        model.addAttribute("user", userDetails);
+        return "user";
+    }
+
+    @GetMapping("/admin-page")
+    public String adminPage (Model model, Principal principal) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(principal.getName());
+        model.addAttribute("user", userDetails);
+        return "admin";
+    }
+}
